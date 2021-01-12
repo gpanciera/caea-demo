36e6db3d33b40daa4e49be1f6fa95c753f18ef05
Greg Panciera <gregpanciera@gmail.com>
2021-01-11 20:00:46 -0600

    initial commit

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+  render() {
+
+    if(this.props.data){
+      var projects = this.props.data.projects.map(function(projects){
+        var projectImage = 'images/portfolio/'+projects.image;
+        return <div key={projects.title} className="gp-portfolio-item">
+           <div className="gp-content-wrapper">
+            <a href={projects.url} title={projects.title}>
+               <img alt={projects.title} src={projectImage} />
+               <div className="gp-overlay">
+                  <div className="gp-portfolio-item-meta">
+                 <h5>{projects.title}</h5>
+                     <p>{projects.category}</p>
+                  </div>
+                </div>
+              {/* <div className="link-icon"><i className="fa fa-link"></i></div> */}
+            </a>
+          </div>
+        </div>
+      })
+    }
+
+    return (
+      <section id="portfolio">
+      <div className="row">
+         <div className="twelve columns collapsed">
+            <div className="gp-portfolio-wrapper">
+                {projects}
+            </div>
+          </div>
+      </div>
+     </section>
+    );
+  }
+}
+
+export default Portfolio;
